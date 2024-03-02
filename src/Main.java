7530732818b8fb4decb4b9cb31bfdf370a330824
Maksim <raymax.spb@gmail.com>
2024-03-02 12:01:57 +0300

    Домашнее задание 7

--- src/Main.java
+++ src/Main.java
@@ -20,21 +20,21 @@ public class Main {
         for (a = 10; a >= 1; a--) {
             System.out.print(a + " ");
         }
-
+        System.out.println(" ");
         System.out.println("Task 3");
-        int people = 12_000_000;
-        int difference = people / 1000;
-        int birth = 17;
-        int death = 8;
+        int people = 12_000_000; // начальная числинность
+        int birthRate = 17; // рождаемость
+        int deathRate = 8; // смертность
         for (int year = 1; year <= 10; year++) {
-            people = people + (difference * (birth - death));
+            people+=people*(birthRate-deathRate)/1000;
+
             System.out.println("Год " + year + " численность населения составляет " + people);
         }
         System.out.println("Task 4");
         int cash = 15000;
-        int profit = cash % 7;
+        int profit = cash/100*7;
         int total3 = 0;
-        int year = 0;
+        int year = 1;
         for (; total3 < 12_000_000; year++) {
             total3 = total3 + profit;
             total3 = total3 + cash;
@@ -42,7 +42,7 @@ public class Main {
         }
         System.out.println("Task 5");
         int cash1 = 15000;
-        int profit1 = cash % 7;
+        int profit1 = cash/100*7;
         int total4 = 0;
         int year1 = 0;
         for (; total4 < 12_000_000; year1++) {
@@ -60,7 +60,7 @@ public class Main {
         for (; month <= 108; month = month + 1.5) {
             total5 = total5 + profit2;
             total5 = total5 + cash2;
-            if (month % 1.5 == 0) {
+            if (month % 6 == 0) {
                 System.out.println(month + " " + total5);
             }
         }
